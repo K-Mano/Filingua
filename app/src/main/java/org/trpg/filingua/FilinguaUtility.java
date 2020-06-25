835ff28cd4b66d2f5feb75ee31e8da55718154f9
K-Mano <39230424+K-Mano@users.noreply.github.com>
2020-06-25 21:26:50 +0900

    Add Utility Class

---
+++ app/src/main/java/org/trpg/filingua/FilinguaUtility.java
@@ -0,0 +1,29 @@
+package org.trpg.filingua;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FilinguaUtility {
+    private class FileInfo{
+        FileInfo(String path, int size){
+            this.path = new File(path);
+            this.size = size;
+        }
+        File path;
+        int size;
+    }
+    private class Directory{
+        Directory(String path, int size, ArrayList<FileInfo> list){
+            this.path = path;
+            this.size = size;
+            fileList = list;
+        }
+        String path;
+        int size;
+        ArrayList<FileInfo> fileList;
+    }
+    public FileInfo setFileInfo(String filepath,int filesize){
+        FileInfo info = new FileInfo(filepath, filesize);
+        return info;
+    }
+}
