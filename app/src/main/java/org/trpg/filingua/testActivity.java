33188ce0b3f68c0aa0dabdf762e95e4b9fc50e17
mionsyun <65840867+mionsyun@users.noreply.github.com>
2020-06-30 21:13:18 +0900

    tab.xmlの変更
    角煮お願いします

---
+++ app/src/main/java/org/trpg/filingua/testActivity.java
@@ -0,0 +1,17 @@
+package org.trpg.filingua;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.trpg.filingua.R;
+
+public class testActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pinned_tab);
+
+    }
+}
