0cbf0e121ff234a71de5a3705f11c84a22112e66
mionsyun <65840867+mionsyun@users.noreply.github.com>
2020-07-14 12:58:21 +0900

    onegaisimasu
    onegasimasu

---
+++ app/src/main/java/org/trpg/filingua/Filingainfo.java
@@ -0,0 +1,13 @@
+package org.trpg.filingua;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Filingainfo {
+
+    static class tabCard {
+        String title;
+        Date date;
+    }
+}
