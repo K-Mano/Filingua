e037e8454f82bac98d711dc552cd5e98ba118eca
K-Mano <39230424+K-Mano@users.noreply.github.com>
2020-07-22 15:09:39 +0900

    Add StorageManager

--- app/src/main/java/org/trpg/filingua/RecentsTabFragment.java
+++ app/src/main/java/org/trpg/filingua/RecentsTabFragment.java
@@ -5,12 +5,23 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.usage.StorageStatsManager;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
+import android.os.storage.StorageManager;
+import android.os.storage.StorageVolume;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class RecentsTabFragment extends Fragment{
 
@@ -19,8 +30,9 @@ public class RecentsTabFragment extends Fragment{
         // Viewを設定
         View rootView = inflater.inflate(R.layout.recents_tab, container, false);
         // RecyclerViewを取得
-        RecyclerView recyclerView = rootView.findViewById(R.id.quickaccess);
-        RecyclerView recentView   = rootView.findViewById(R.id.recent_activity);
+        RecyclerView recyclerView  = rootView.findViewById(R.id.quickaccess);
+        RecyclerView recentView    = rootView.findViewById(R.id.recent_activity);
+        RecyclerView driveListView = rootView.findViewById(R.id.drive_list);
         // LayoutManagerを設定
         LinearLayoutManager lManager = new LinearLayoutManager(getActivity());
         lManager.setOrientation(LinearLayoutManager.HORIZONTAL);
@@ -29,15 +41,22 @@ public class RecentsTabFragment extends Fragment{
         LinearLayoutManager vManager = new LinearLayoutManager(getActivity());
         vManager.setOrientation(LinearLayoutManager.VERTICAL);
         recentView.setLayoutManager(vManager);
+        recentView.setHasFixedSize(true);
+        LinearLayoutManager dManager = new LinearLayoutManager(getActivity());
+        dManager.setOrientation(LinearLayoutManager.VERTICAL);
+        driveListView.setLayoutManager(dManager);
+        driveListView.setHasFixedSize(true);
+        //Dividerの設定
         DividerItemDecoration divider = new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL);
         recentView.addItemDecoration(divider);
-        recentView.setHasFixedSize(true);
         // Adapterを作成
-        RecyclerAdapter rAdapter = new RecyclerAdapter(createObject(), R.layout.qa_card);
-        RecyclerAdapter aAdapter = new RecyclerAdapter(createObject(), R.layout.ra_card);
+        RecyclerAdapter rAdapter = new RecyclerAdapter(createObject(3), R.layout.qa_card);
+        RecyclerAdapter aAdapter = new RecyclerAdapter(createObject(5), R.layout.ra_card);
+        DiskInfoAdapter dAdapter = new DiskInfoAdapter(getVolumeInfo());
         // Adapterを設定
         recyclerView.setAdapter(rAdapter);
         recentView.setAdapter(aAdapter);
+        driveListView.setAdapter(dAdapter);
         return rootView;
     }
 
@@ -46,13 +65,60 @@ public class RecentsTabFragment extends Fragment{
         super.onViewCreated(view, savedInstanceState);
     }
 
-    private List<FilinguaDatabase.CardObject> createObject(){
-        List<FilinguaDatabase.CardObject> cardObjectList = new ArrayList<>();
-        for(int i=0;i<4;i++){
-            FilinguaDatabase.CardObject object = new FilinguaDatabase.CardObject();
-            object.title="card"+i;
-            cardObjectList.add(object);
+    private List<FilinguaDatabase.DefaultDataSet> createObject(int amount){
+        List<FilinguaDatabase.DefaultDataSet> dataSet = new ArrayList<>();
+        for(int i=0;i<amount;i++){
+            FilinguaDatabase.DefaultDataSet object = new FilinguaDatabase.DefaultDataSet("card"+i);
+            dataSet.add(object);
         }
-        return cardObjectList;
+        return dataSet;
+    }
+
+    private List<FilinguaDatabase.DiskInfoDataSet> getVolumeInfo() {
+        StorageStatsManager sStatsManager = MainActivity.getStorageStatsManager();
+        StorageManager sManager = MainActivity.getsManager();
+
+        List<FilinguaDatabase.DiskInfoDataSet> dataSet = new ArrayList<>();
+        List<StorageVolume> volumes = sManager.getStorageVolumes();
+        List<UUID> uuid = new ArrayList<>();
+
+        float used;
+        float total;
+
+        // GB(ギガバイト)
+        final int GB = (int)Math.pow(1024, 3);
+
+        //デバッグ用コード
+        Log.d("StorageManager", "Starting volume information updates");
+        Log.d("StorageManager", "############### VOLUME LIST ###############");
+        for(int count=0; count<volumes.size(); count++){
+            Log.d("StorageManager", String.format("Volume %d: \"%s\", Status=\"%s\"", count, volumes.get(count).getDescription(getContext()), volumes.get(count).getState()));
+        }
+        Log.d("StorageManager", "###########################################");
+        //--------------
+        for(int count=0; count<volumes.size(); count++){
+            try {
+                if(volumes.get(count).isPrimary()){
+                    Log.d("StorageManager", String.format("Volume %d is primary storage.", count));
+                    uuid.add(StorageManager.UUID_DEFAULT);
+                }else{
+                    Log.d("StorageManager", String.format("Volume %d is removable storage.", count));
+                    uuid.add(UUID.nameUUIDFromBytes(volumes.get(count).getUuid().getBytes()));
+                }
+                Log.d("StorageManager", String.format("Volume UUID is \"%s\"", uuid.get(count).toString()));
+                total = sStatsManager.getTotalBytes(uuid.get(count));
+                used  = total - (sStatsManager.getFreeBytes(uuid.get(count)));
+            }catch (Exception e){
+                Log.d("StorageManager", String.format("Error in Volume %d: %s",count ,e));
+                total = 0;
+                used  = 0;
+            }
+
+            Log.d("StorageManager", String.format("Volume %d %s.",count,volumes.get(0).getState()));
+            dataSet.add(new FilinguaDatabase.DiskInfoDataSet(volumes.get(count).getDescription(getContext()), total/GB, used/GB, volumes.get(count).isPrimary(), volumes.get(count).isRemovable()));
+        }
+
+        Log.d("StorageManager", String.format("Volume information has been successfully updated."));
+        return dataSet;
     }
 }
